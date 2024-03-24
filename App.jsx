00eb3d21e18cce4379a1b151fd8b48633be03753
linguanghui <linguanghui@cvte.com>
2024-03-24 11:53:24 +0800

    feat: 支持tsx

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from './core/React.js'
+
+// const App = React.createElement('div', {id: 'app'}, 'Hello World', 'Hello World 2', React.createElement("div", {id: 'app2'}, 'Hello World 3'));
+
+ const App = <div>123</div>
+
+
+export default App;
